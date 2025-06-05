f43708d778b3d6002037da94a6b954fa0ff745df
winter <annisaallyasalsabillahh@gmail.com>
2025-06-05 21:24:20 +0700

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:fasum/firebase_options.dart';
 import 'package:fasum/screens/splash_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart'; 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
